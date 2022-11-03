6a9cceb809424c48a87d5018bf9bfa3f89ba5139
Gevorg <gevacreature2016@mail.ru>
2022-11-03 23:07:49 +0300

    DZ4

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите три числа");
+string число1 = Console.ReadLine();
+int a = int.Parse(число1);
+string число2 = Console.ReadLine();
+int b = int.Parse(число2);
+string число3 = Console.ReadLine();
+int C = int.Parse(число3);
+
+if (a > b)
+{
+    if (a > C)
+    {
+    
+        Console.WriteLine(a);
+    }
+    else
+{
+    Console.WriteLine(C);
+}
+}
+else
+{
+    if(b > C)
+
+    {Console.WriteLine(b); 
+}
+
+else
+{
+    Console.WriteLine(C);
+}}
