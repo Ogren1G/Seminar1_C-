6ec9e1c511f9b65f05aa74c945cad8ee04c62013
Gevorg <gevacreature2016@mail.ru>
2022-11-03 21:03:46 +0300

    DZ2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа");
+string число1 = Console.ReadLine();
+int a = int.Parse(число1);
+string число2 = Console.ReadLine();
+int b = int.Parse(число2);
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
