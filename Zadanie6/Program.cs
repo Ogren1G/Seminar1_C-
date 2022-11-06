3ffe8d1f5d0ab4ec1c21427e799f8ffbc5df1e41
Gevorg <gevacreature2016@mail.ru>
2022-11-06 14:23:47 +0300

    Full DZ

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string число = Console.ReadLine();
+double a = double.Parse(число);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ Zadanie8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string a = Console.ReadLine();
+int n = int.Parse(a);
+int j = 1;
+
+while (j <= n)
+{
+    if(j % 2 == 0)
+    {
+        Console.WriteLine(j);
+    }
+    j++;}
